aaca6a5bb922b92e984eaa3a8d2e1a5b2eee389b
Connor Newton <connor@ifthenelse.io>
2017-07-23 17:51:16 +0100

    Replace recursive topological sort with iterative algorithm

--- src/TopologicalSort.php
+++ src/TopologicalSort.php
@@ -19,47 +19,56 @@ class TopologicalSort extends BaseGraph
     /**
      * run algorithm and return an ordered/sorted set of Vertices
      *
-     * the topologic sorting may be non-unique depending on your edges. this
-     * algorithm tries to keep the order of vertices as added to the graph in
-     * this case.
+     * the topological sorting may be non-unique depending on your edges
      *
      * @return Vertices
      */
     public function getVertices()
     {
-        $tsl = array();
+        $stack = array(); // visited nodes with unvisited children
         $visited = array();
+        $output = array();
 
-        // TODO: find alternative to recursive algorithm to avoid hitting recursion limit with ~100 nodes
+        /** @var Vertex $top */
         // TODO: avoid having to reverse all vertices multiple times
-
-        foreach(array_reverse($this->graph->getVertices()->getVector()) as $vertex) {
-            $this->visit($vertex, $visited, $tsl);
-        }
-
-        return new Vertices(array_reverse($tsl, true));
-    }
-
-    protected function visit(Vertex $vertex, array &$visited, array &$tsl)
-    {
-        $vid = $vertex->getId();
-        if (isset($visited[$vid])) {
-            if ($visited[$vid] === false) {
-                // temporary mark => not a DAG
-                throw new UnexpectedValueException('Not a DAG');
-            }
-            // otherwise already marked/visisted => no need to check again
-        } else {
-            // temporary mark
-            $visited[$vid] = false;
-
-            foreach (array_reverse($vertex->getVerticesEdgeTo()->getVector()) as $v) {
-                $this->visit($v, $visited, $tsl);
+        // pick a node to examine next - assume there are isolated nodes
+        foreach (array_reverse($this->graph->getVertices()->getVector()) as $top) {
+            $tid = $top->getId();
+            if (!isset($visited[$tid])) { // don't examine if already found
+                array_push($stack, $top);
             }
 
-            // mark as visited and include in result
-            $visited[$vid] = true;
-            $tsl[$vid] = $vertex;
+            while ($stack) {
+                /** @var Vertex $current */
+                $node = end($stack);
+                $nid = $node->getId();
+
+                $visited[$nid] = false; // temporary mark
+                $found = false; // new children found during visit to this node
+
+                // find the next node to visit
+                /** @var Vertex $child */
+                foreach (array_reverse($node->getVerticesEdgeTo()->getVector()) as $child) {
+                    $cid = $child->getId();
+                    if (!isset($visited[$cid])) {
+                        // found a new node - push it onto the stack
+                        array_push($stack, $child);
+                        $found = true; // move onto the new node
+                        break;
+                    } else if ($visited[$cid] === false) {
+                        // temporary mark => not a DAG
+                        throw new UnexpectedValueException('Not a DAG');
+                    }
+                }
+
+                if (!$found) {
+                    array_pop($stack); // no new children found - we're done with this node
+                    $visited[$nid] = true; // mark as visited
+                    array_push($output, $node); // add to results
+                }
+            }
         }
+
+        return new Vertices(array_reverse($output, true));
     }
 }
